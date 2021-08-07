e04ce7d6f260e5d8998754498d02676b3bec35a5
edr2694 <edr2694@g.rit.edu>
2021-08-06 23:43:08 -0400

    -- codebase cleanup -- added gitignore -- "finalized" GUI look and feel

--- rgbClasses.py
+++ rgbClasses.py
@@ -2,14 +2,25 @@ from tkinter import *
 import os.path
 
 
-fileLoc = "/home/pi/"
-
-defaults = ["disabled", "off", "100,0,0", "1", "1", "0"]
+fileLoc = ""
+# example file below
+"""
+disabled
+off
+100,0,0
+1
+1
+0
+"""
+defaults = ["disabled", "qsSolid", "100,0,0", "1", "1", "0"]
+# need disabled because somthing has to show up on screen for initial conditions
+stateList = ["qsSolid", "solid", "breathing", "flashingMult", "colorShift"]
 
 def createFile(filename, startDisabled):
     if (os.path.isfile(filename)):
-        print("creating file " + filename)
+        print("reading file " + filename)
         if (startDisabled):
+            print("disabling channel associated with " + filename)
             f = open(filename, 'r')
             lines = f.readlines()
             lines[0] = "disabled\n"
@@ -18,6 +29,7 @@ def createFile(filename, startDisabled):
             f.writelines("%s" % line for line in lines)
             f.close()
     else:
+        print("creating file " + filename)
         f = open(filename,'w')
         f.writelines("%s\n" % line for line in defaults)
         f.close()
@@ -34,10 +46,10 @@ class RGBChannel:
         self.stateFile = str(fileLoc + name + "_state.txt")
         writelines = defaults;
         createFile(self.stateFile, startDisabled)
-        f = open(self.stateFile, 'r')
-        lines = f.readlines()
+        with open(self.stateFile) as f:
+            lines = f.read().splitlines()
         f.close()
-        self.enabled = lines[0]
+        self.enabled = bool(lines[0] == "enabled")
         self.state = lines[1]
         currentColors = list(lines[2].split(","))
         self.red = int(currentColors[0])
@@ -49,14 +61,14 @@ class RGBChannel:
         self.button = None
         self.colorList=colorList
         self.colorIndex=0
-        if (self.enabled == "enabled" and not startDisabled):
+        if (self.enabled):
             self.buttonState = "inactiveOn"
         else:
             self.buttonState = "inactiveOff"
 
     def writeProperty(self, prop, value):
-        f = open(self.stateFile, 'r')
-        lines = f.readlines()
+        with open(self.stateFile) as f:
+            lines = f.read().splitlines()
         f.close()
         index = 0
         if (prop == "enable"):
@@ -72,31 +84,45 @@ class RGBChannel:
         elif (prop == "qscolor"):
             index = 5
         else:
-            printf("error! invalid property change requested")
+            print("error! invalid property change requested")
             return
         lines[index] = value
-        f = open(filename,'w')
+        f = open(self.stateFile,'w')
         f.writelines("%s\n" % line for line in lines)
         f.close()
-            
+
+    def readValues(self):
+        with open(self.stateFile) as f:
+            lines = f.read().splitlines()
+        f.close()
+        self.enabled = bool(lines[0] == "enabled")
+        self.state = lines[1]
+        currentColors = list(lines[2].split(","))
+        self.red = int(currentColors[0])
+        self.green = int(currentColors[1])
+        self.blue = int(currentColors[2])
+        self.speed = int(lines[3])
+        self.brightness = int(lines[4])
+        self.qsIndex = int(lines[5])
+
     def enableChannel(self):
         self.enabled = True
         self.writeProperty("enable", "enabled")
-        
+
     def disableChannel(self):
         self.enabled = False
         self.writeProperty("enable", "disabled")
-     
+
     def setState(self, value):
         self.state = value
         self.writeProperty("state", value)
-        
+
     def setColos(self, red, green, blue):
          self.red = red
          self.green = green
          self.blue = blue
          self.writeProperty(color, str(str(red) + "," + str(green) + "," + str(blue)))
-     
+
     def cycleQSColors(self):
         self.colorIndex = self.colorIndex + 1
         if (self.colorIndex >= len(self.colorList)):
@@ -106,13 +132,7 @@ class RGBChannel:
     def setSpeed(self, value):
         self.speed = value
         self.writeProperty("speed", str(value))
-    
+
     def setBrightness(self, value):
         self.brightness = value
         self.writeProperty(self, str(value))
-        
-        
-        
-        
-        
-        
--- rgbControlPanel.py
+++ rgbControlPanel.py
@@ -30,9 +30,11 @@ class RGBControlPanel:
         self.footwellChannel = RGBChannel(name="Footwell", startDisabled=False, colorList=colorList)
         self.shifterChannel = RGBChannel(name="Shifter", startDisabled=False, colorList=colorList)
         self.underglowChannel = RGBChannel(name="Underglow", startDisabled=True, colorList=colorList)
+        self.channels = [self.footwellChannel, self.shifterChannel, self.underglowChannel]
+        self.activeChannel = self.channels[0]
         self.window = Tk()
         self.window.title = "RGB Control"
-        self.window.attributes('-zoomed', True)
+        #self.window.attributes('-zoomed', True)
         self.window.rowconfigure((0,1,2,3,4,5,6,7,8), weight=1)
         self.window.columnconfigure(0, weight=1)
         topBtnFrame = Frame(self.window)
@@ -41,35 +43,36 @@ class RGBControlPanel:
         topBtnFrame.grid(row=0, column=0, sticky='nesw')
         self.footwellChannel.button = Button(topBtnFrame, text="FOOTWELL", font=("Conduit ITC",32), command=lambda: self.channelButtonPress(self.footwellChannel.button), highlightcolor="blue")
         self.footwellChannel.button.config(activebackground=self.footwellChannel.button.cget('background'), activeforeground=self.footwellChannel.button.cget('foreground'))
+        self.footwellChannel.button.config(bg="#363636", bd=10, fg="white")
         self.footwellChannel.button.grid(row=0, column=0, sticky='nesw')
         self.shifterChannel.button = Button(topBtnFrame, text="SHIFTER", font=("Conduit ITC",32), command=lambda: self.channelButtonPress(self.shifterChannel.button))
         self.shifterChannel.button.config(activebackground=self.shifterChannel.button.cget('background'), activeforeground=self.shifterChannel.button.cget('foreground'))
+        self.shifterChannel.button.config(bg="#363636", bd=10, fg="white")
         self.shifterChannel.button.grid(row=0,column=1, sticky='nesw')
         self.underglowChannel.button = Button(topBtnFrame, text="UNDERGLOW", font=("Conduit ITC",32), command=lambda: self.channelButtonPress(self.underglowChannel.button))
         self.underglowChannel.button.config(activebackground=self.underglowChannel.button.cget('background'), activeforeground=self.underglowChannel.button.cget('foreground'))
+        self.underglowChannel.button.config(bg="#363636", bd=10, fg="white")
         self.underglowChannel.button.grid(row=0,column=2, sticky='nesw')
-        self.activechannel = self.footwellChannel
-        self.channels = [self.footwellChannel, self.shifterChannel, self.underglowChannel]
 
-        self.redSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50, troughcolor="red", repeatdelay=100, repeatinterval=35, command=lambda: self.updateSlider(self.redSlider))
+        self.redSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50, bg="#363636", fg="white", troughcolor="red", repeatdelay=100, repeatinterval=35, command=lambda: self.updateSlider(self.redSlider))
         self.redSlider.grid(row=1, column=0, sticky='news')
 
-        self.greenSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50, troughcolor="green")
+        self.greenSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50, bg="#363636", fg="white", troughcolor="green", repeatdelay=100, repeatinterval=35, command=lambda: self.updateSlider(self.redSlider))
         self.greenSlider.grid(row=2, column=0, sticky='nesw')
 
-        self.blueSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50, troughcolor="blue")
+        self.blueSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50, bg="#363636", fg="white", troughcolor="blue", repeatdelay=100, repeatinterval=35, command=lambda: self.updateSlider(self.redSlider))
         self.blueSlider.grid(row=3, column=0, sticky='nesw')
-        
-        self.speedLabel = Label(self.window, text="Animation Speed", font=("Conduit ITC",24))
+
+        self.speedLabel = Label(self.window, text="Animation Speed", fg="white", bg="#363636", font=("Conduit ITC",24))
         self.speedLabel.grid(row=4, column=0, sticky='nesw')
 
-        self.speedSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50)
+        self.speedSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50, bg="#363636", fg="white", troughcolor="black", repeatdelay=100, repeatinterval=35, command=lambda: self.updateSlider(self.redSlider))
         self.speedSlider.grid(row=5, column=0, sticky='nesw')
 
-        self.brightLabel = Label(self.window, text="Brightness", font=("Conduit ITC",24))
+        self.brightLabel = Label(self.window, text="Brightness", fg="white", bg="#363636", font=("Conduit ITC",24))
         self.brightLabel.grid(row=6, column=0, sticky='nesw')
 
-        self.brightnessSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50)
+        self.brightnessSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50, bg="#363636", fg="white",  troughcolor="black", repeatdelay=100, repeatinterval=35, command=lambda: self.updateSlider(self.redSlider))
         self.brightnessSlider.grid(row=7, column=0, sticky='nesw')
 
         self.botBtnFrame = Frame(self.window)
@@ -78,53 +81,80 @@ class RGBControlPanel:
         self.botBtnFrame.grid(row=8, column=0, sticky='nesw')
 
         self.sqsBtn = Button(self.botBtnFrame, text="Solid Quick \nSelect", font=("Conduit ITC",32))
+        self.sqsBtn.config(bg="#363636", bd=10, fg="white")
         self.sqsBtn.grid(row=0, column=0, sticky='nesw')
 
         self.solidBtn = Button(self.botBtnFrame, text="Solid", font=("Conduit ITC",32))
+        self.solidBtn.config(bg="#363636", bd=10, fg="white")
         self.solidBtn.grid(row=0, column=1, sticky='nesw')
 
         self.breathBtn = Button(self.botBtnFrame, text="Breathing", font=("Conduit ITC",32))
+        self.breathBtn.config(bg="#363636", bd=10, fg="white")
         self.breathBtn.grid(row=0, column=2, sticky='nesw')
 
         self.flashBtn = Button(self.botBtnFrame, text="Flashing Multicolor", font=("Conduit ITC",32))
+        self.flashBtn.config(bg="#363636", bd=10, fg="white")
         self.flashBtn.grid(row=0, column=3, sticky='nesw')
 
         self.spectrumBtn = Button(self.botBtnFrame, text="Color Shift", font=("Conduit ITC",32))
+        self.spectrumBtn.config(bg="#363636", bd=10, fg="white")
         self.spectrumBtn.grid(row=0, column=4, sticky='nesw')
+
+        for chan in self.channels:
+            chan.readValues()
+            if (chan.enabled):
+                chan.button.config(fg="white", relief=SUNKEN, activeforeground="white", bg='#363636')
+
         self.window.mainloop()
 
 #    def switchActiveChannel(self, channel):
-        # read channel file
-        
-#    def updateSlider(self, slider):
-        
+#        # pull last values from file
+#        self.channel.readValues()
+#
+
+#    def sliderCallback(self, slider):
+
     def channelButtonPress(self, inbutton):
+        # save values
         for chan in self.channels:
             if (inbutton == chan.button):
+                # Do work on the button we just pushed
                 if (chan.buttonState == "inactiveOff"):
+                    # Button was not previously pressed, we've pressed it once to display the channel
                     chan.buttonState = "activeOff"
-                    chan.button.config(fg="blue", relief=GROOVE, activeforeground="blue")
-                elif (chan.buttonState == "inactiveOn" or chan.buttonState == "activeOff"):
+                    chan.button.config(fg="red", relief=GROOVE, activeforeground="red", bg='#363636')
+                    # switchActiveChannel(chan)
+                elif (chan.buttonState == "inactiveOn"):
+                    # we're coming back to an already turned on channel
                     chan.buttonState = "activeOn"
                     self.activeChannel = chan
-                    chan.button.config(fg="blue", relief=SUNKEN, activeforeground="blue")
+                    chan.button.config(fg="red", relief=SUNKEN, activeforeground="red", bg='#363636')
+                elif (chan.buttonState == "activeOff"):
+                    # we just pressed the button to turn on the channel
+                    chan.buttonState = "activeOn"
+                    chan.button.config(fg="red", relief=SUNKEN, activeforeground="red", bg='#363636')
+                    chan.enableChannel()
                 elif (chan.buttonState == "activeOn"):
+                    # We just pressed the button to turn off the channel
                     chan.buttonState = "activeOff"
-                    self.activeButton = inbutton
                     self.activeChannel = chan
-                    chan.button.config(fg="blue", relief=GROOVE,activeforeground="blue")
+                    chan.button.config(fg="red", relief=GROOVE,activeforeground="red", bg='#363636')
+                    chan.disableChannel()
                 else:
+                    # I don't think we should ever get to this point
                     chan.buttonState = "inactiveOff"
-                    chan.button.config(fg="black", relief=RAISED,activeforeground="black")
-            else: # button was not pressed
+                    chan.button.config(fg="white", relief=RAISED,activeforeground="green", bg='#363636')
+            else:
+                # Do work on the buttons we didn't press
                 if (chan.buttonState == "activeOn"):
                     chan.buttonState = "inactiveOn"
-                    chan.button.config(fg="black", relief=SUNKEN, activeforeground="black")
+                    chan.button.config(fg="white", relief=SUNKEN, activeforeground="white", bg='#363636')
                 elif (chan.buttonState == "activeOff"):
                     chan.buttonState = "inactiveOff"
-                    chan.button.config(fg="black", relief=RAISED,activeforeground="black")
+                    chan.button.config(fg="white", relief=RAISED,activeforeground="white", bg='#363636')
                 else: # inactives, state=state, no changes
                     continue
+        #load values for new active channel
         self.window.focus_set()
 
 if __name__ == "__main__":
