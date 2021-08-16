1bb5f769bf716237f50e013e9aa7cdd62dbc33cc
edr2694 <edr2694@g.rit.edu>
2021-08-15 22:48:14 -0400

    rgbClasses: -- bug fixes rgbControlPanel: -- completed state button implementation -- added scale disabling in certain states -- fixed colors to be on a 1-100 scale

--- rgbControlPanel.py
+++ rgbControlPanel.py
@@ -54,19 +54,22 @@ class RGBControlPanel:
         self.underglowChannel.button.config(bg="#363636", bd=10, fg="white")
         self.underglowChannel.button.grid(row=0,column=2, sticky='nesw')
 
+        self.rsv = IntVar()
         self.redSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50,
             bg="#363636", fg="white", troughcolor="red", repeatdelay=100, repeatinterval=35,
-            command=lambda pos: self.sliderCB("red", pos))
+            command=lambda pos: self.sliderCB("red", pos), variable=self.rsv)
         self.redSlider.grid(row=1, column=0, sticky='news')
 
+        self.gsv = IntVar()
         self.greenSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50,
             bg="#363636", fg="white", troughcolor="green", repeatdelay=100, repeatinterval=35,
-            command=lambda pos: self.sliderCB("green", pos))
+            command=lambda pos: self.sliderCB("green", pos), variable=self.gsv)
         self.greenSlider.grid(row=2, column=0, sticky='nesw')
 
+        self.bsv = IntVar()
         self.blueSlider = Scale(self.window, from_=0, to=100, orient=HORIZONTAL, width=50,
             bg="#363636", fg="white", troughcolor="blue", repeatdelay=100, repeatinterval=35,
-            command=lambda pos: self.sliderCB("blue", pos))
+            command=lambda pos: self.sliderCB("blue", pos), variable=self.bsv)
         self.blueSlider.grid(row=3, column=0, sticky='nesw')
 
         self.speedLabel = Label(self.window, text="Animation Speed", fg="white", bg="#363636", font=("Conduit ITC",24))
@@ -90,23 +93,24 @@ class RGBControlPanel:
         self.botBtnFrame.columnconfigure((0,1,2,3,4), weight=1)
         self.botBtnFrame.grid(row=8, column=0, sticky='nesw')
 
-        self.sqsBtn = Button(self.botBtnFrame, text="Solid Quick \nSelect", font=("Conduit ITC",32), command=self.qsButtonPress)
+        sqsString = "Solid Quick     \nSelect: " + self.activeChannel.colorList[self.activeChannel.qsIndex].name
+        self.sqsBtn = Button(self.botBtnFrame, text=sqsString, font=("Conduit ITC",32), command=lambda: self.qsButtonPress(False), justify="left")
         self.sqsBtn.config(bg="#363636", bd=10, fg="white")
         self.sqsBtn.grid(row=0, column=0, sticky='nesw')
 
-        self.solidBtn = Button(self.botBtnFrame, text="Solid", font=("Conduit ITC",32))
+        self.solidBtn = Button(self.botBtnFrame, text="Solid", font=("Conduit ITC",32), command=self.solidButtonPress)
         self.solidBtn.config(bg="#363636", bd=10, fg="white")
         self.solidBtn.grid(row=0, column=1, sticky='nesw')
 
-        self.breathBtn = Button(self.botBtnFrame, text="Breathing", font=("Conduit ITC",32))
+        self.breathBtn = Button(self.botBtnFrame, text="Breathing", font=("Conduit ITC",32), command=self.breathButtonPress)
         self.breathBtn.config(bg="#363636", bd=10, fg="white")
         self.breathBtn.grid(row=0, column=2, sticky='nesw')
 
-        self.flashBtn = Button(self.botBtnFrame, text="Flashing Multicolor", font=("Conduit ITC",32))
+        self.flashBtn = Button(self.botBtnFrame, text="Flashing\nMulticolor", font=("Conduit ITC",32), command=self.multiFlashButtonPress)
         self.flashBtn.config(bg="#363636", bd=10, fg="white")
         self.flashBtn.grid(row=0, column=3, sticky='nesw')
 
-        self.spectrumBtn = Button(self.botBtnFrame, text="Color Shift", font=("Conduit ITC",32))
+        self.spectrumBtn = Button(self.botBtnFrame, text="Color Shift", font=("Conduit ITC",32), command=self.colorShiftButtonPress)
         self.spectrumBtn.config(bg="#363636", bd=10, fg="white")
         self.spectrumBtn.grid(row=0, column=4, sticky='nesw')
 
@@ -145,9 +149,12 @@ class RGBControlPanel:
             val = pos
             self.activeChannel.brightness = pos
         self.activeChannel.writeProperty(prop, val)
+        if (self.activeChannel.state == "qsSolid"):
+            self.solidButtonPress()
 
     def channelButtonPress(self, inbutton):
         # save values
+        channelChanged = False
         for chan in self.channels:
             if (inbutton == chan.button):
                 # Do work on the button we just pushed
@@ -157,12 +164,14 @@ class RGBControlPanel:
                     chan.button.config(fg="red", relief=GROOVE, activeforeground="red", bg='#363636')
                     self.activeChannel.writeAll()
                     self.activeChannel = chan
+                    channelChanged = True
                 elif (chan.buttonState == "inactiveOn"):
                     # we're coming back to an already turned on channel
                     chan.buttonState = "activeOn"
                     self.activeChannel.writeAll()
                     self.activeChannel = chan
                     chan.button.config(fg="red", relief=SUNKEN, activeforeground="red", bg='#363636')
+                    channelChanged = True
                 elif (chan.buttonState == "activeOff"):
                     # we just pressed the button to turn on the channel
                     chan.buttonState = "activeOn"
@@ -188,6 +197,18 @@ class RGBControlPanel:
                     chan.button.config(fg="white", relief=RAISED,activeforeground="white", bg='#363636')
                 else: # inactives, state=state, no changes
                     continue
+            if (channelChanged):
+                chan.readValues()
+                if (self.activeChannel.state == "qsSolid"):
+                    self.qsButtonPress(True)
+                elif (self.activeChannel.state == "solid"):
+                    self.solidButtonPress()
+                elif (self.activeChannel.state == "breathing"):
+                    self.breathButtonPress()
+                elif (self.activeChannel.state == "flashingMult"):
+                    self.multiFlashButtonPress()
+                elif (self.activeChannel.state == "colorShift"):
+                    self.colorShiftButtonPress()
         # set slider values
         self.redSlider.set(self.activeChannel.red)
         self.greenSlider.set(self.activeChannel.green)
@@ -196,27 +217,103 @@ class RGBControlPanel:
         self.brightnessSlider.set(self.activeChannel.brightness)
         self.window.focus_set()
 
-    def qsButtonPress(self):
-        self.activeChannel.cycleQSColors()
-        self.sqsBtn.config(fg="white", relief=SUNKEN, activeforeground="white", bg='#363636')
+    def qsButtonPress(self, activeChannelLoad):
+        if (self.activeChannel.state == "qsSolid" and not activeChannelLoad):
+            self.activeChannel.cycleQSColors()
+        else:
+            self.activeChannel.red = self.activeChannel.colorList[self.activeChannel.qsIndex].red
+            self.activeChannel.green = self.activeChannel.colorList[self.activeChannel.qsIndex].green
+            self.activeChannel.blue = self.activeChannel.colorList[self.activeChannel.qsIndex].blue
+        sqsString = "Solid Quick     \nSelect: " + self.activeChannel.colorList[self.activeChannel.qsIndex].name
+        self.sqsBtn.config(fg="white", relief=SUNKEN, activeforeground="white", bg='#363636', text=sqsString, justify="left")
         self.solidBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.breathBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.flashBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.spectrumBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.activeChannel.state = "qsSolid"
+        self.rsv.set(self.activeChannel.red)
+        self.gsv.set(self.activeChannel.green)
+        self.bsv.set(self.activeChannel.blue)
+        self.redSlider.config(state="normal")
+        self.blueSlider.config(state="normal")
+        self.greenSlider.config(state="normal")
+        self.speedSlider.config(state="disabled")
+        self.activeChannel.writeAll()
+
+    def solidButtonPress(self):
+        self.activeChannel.setState("solid")
+        self.sqsBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.solidBtn.config(fg="white", relief=SUNKEN, activeforeground="white", bg='#363636')
         self.breathBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
         self.flashBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
         self.spectrumBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
         self.redSlider.set(self.activeChannel.red)
         self.greenSlider.set(self.activeChannel.green)
         self.blueSlider.set(self.activeChannel.blue)
-        print(str(self.activeChannel.red) + "," + str(self.activeChannel.green) + "," + str(self.activeChannel.blue))
+        self.redSlider.config(state="normal")
+        self.blueSlider.config(state="normal")
+        self.greenSlider.config(state="normal")
+        self.speedSlider.config(state="disabled")
+        self.activeChannel.writeAll()
+
+    def breathButtonPress(self):
+        self.activeChannel.setState("breathing")
+        self.sqsBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.solidBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.breathBtn.config(fg="white", relief=SUNKEN, activeforeground="white", bg='#363636')
+        self.flashBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.spectrumBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.redSlider.set(self.activeChannel.red)
+        self.greenSlider.set(self.activeChannel.green)
+        self.blueSlider.set(self.activeChannel.blue)
+        self.redSlider.config(state="normal")
+        self.blueSlider.config(state="normal")
+        self.greenSlider.config(state="normal")
+        self.speedSlider.config(state="normal")
+        self.activeChannel.writeAll()
+
+    def multiFlashButtonPress(self):
+        self.activeChannel.setState("flashingMult")
+        self.sqsBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.solidBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.breathBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.flashBtn.config(fg="white", relief=SUNKEN, activeforeground="white", bg='#363636')
+        self.spectrumBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.redSlider.set(self.activeChannel.red)
+        self.greenSlider.set(self.activeChannel.green)
+        self.blueSlider.set(self.activeChannel.blue)
+        self.redSlider.config(state="normal")
+        self.blueSlider.config(state="normal")
+        self.greenSlider.config(state="normal")
+        self.speedSlider.config(state="normal")
+        self.activeChannel.writeAll()
+
+    def colorShiftButtonPress(self):
+        self.activeChannel.setState("colorShift")
+        self.sqsBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.solidBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.breathBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.flashBtn.config(fg="white", relief=RAISED, activeforeground="white", bg='#363636')
+        self.spectrumBtn.config(fg="white", relief=SUNKEN, activeforeground="white", bg='#363636')
+        self.redSlider.set(self.activeChannel.red)
+        self.greenSlider.set(self.activeChannel.green)
+        self.blueSlider.set(self.activeChannel.blue)
+        self.redSlider.config(state="disabled")
+        self.blueSlider.config(state="disabled")
+        self.greenSlider.config(state="disabled")
+        self.speedSlider.config(state="normal")
+        self.activeChannel.writeAll()
 
 if __name__ == "__main__":
-    red = RGBColor("red", 255, 0, 0)
-    green = RGBColor("green", 0, 255, 0)
-    blue = RGBColor("blue", 0, 0, 255)
-    yellow = RGBColor("yellow", 255, 255, 0)
-    pink = RGBColor("pink", 255, 0, 255)
-    purple = RGBColor("purple", 128, 0, 255)
-    teal = RGBColor("teal", 0, 255, 255)
-    orange = RGBColor("orange", 255, 128, 0)
+    red = RGBColor("red", 100, 0, 0)
+    green = RGBColor("green", 0, 100, 0)
+    blue = RGBColor("blue", 0, 0, 100)
+    yellow = RGBColor("yellow", 100, 100, 0)
+    pink = RGBColor("pink", 100, 0, 100)
+    purple = RGBColor("purple", 50, 0, 100)
+    teal = RGBColor("teal", 0, 100, 100)
+    orange = RGBColor("orange", 100, 50, 0)
+    white = RGBColor("white", 100, 100, 100)
 
     newColors = [red, green, blue, yellow, pink, purple, teal, orange]
 
